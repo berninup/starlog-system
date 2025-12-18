bd23aa66c2f8aab919e883725fb69103b99300e0
berninup <chris.basham@live.com>
2025-12-18 13:22:02 -0600

    Formatting float digits, updated main

--- main.py
+++ main.py
@@ -15,18 +15,19 @@ def format_asteroid_data(asteroid):
     name = asteroid['name']
     hazardous = asteroid['is_potentially_hazardous_asteroid']
     hazardous_stat = "Hostile Detected" if hazardous else "Safe"
-    diameter_meters = asteroid['estimated_diameter']['meters']['estimated_diameter_max']
-    velocity_kph = asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']
-    print(f"Asteroid {name}: Danger: {hazardous_stat} Size: {diameter_meters} meters Speed: {velocity_kph} kph")
-    return f"Asteroid {name}: Danger: {hazardous_stat} Size: {diameter_meters} meters Speed: {velocity_kph} kph"
+    diameter_meters = float(asteroid['estimated_diameter']['meters']['estimated_diameter_max'])
+    velocity_kph = float(asteroid['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'])
+    return f"Asteroid {name}: Danger: {hazardous_stat} Size: {diameter_meters:.2f} meters Speed: {velocity_kph:.2f} kph"
     
 
 if __name__ == "__main__":
     today = date.today()
     api_key = os.getenv("NASA_API_KEY")
     asteroids = fetch_asteroid_data(api_key, today, today)
-    for asteroid in asteroids['near_earth_objects'][str(today)]:
-        format_asteroid_data(asteroid)
+    todays_asteroids = asteroids['near_earth_objects'][str(today)]
+    for asteroid in todays_asteroids:
+        stat_block = format_asteroid_data(asteroid)
+        print(stat_block)
         
 
 
